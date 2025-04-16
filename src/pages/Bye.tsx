10c9e1b06f04a974cf092799affa447760ac5fdc
Il Tuo Nome <la-tua-email@example.com>
2025-04-16 16:10:40 +0200

    fix: Sostituisci SVG con immagine PNG nella pagina Bye

--- src/pages/Bye.tsx
+++ src/pages/Bye.tsx
@@ -6,9 +6,9 @@ const Bye: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-10rem)] space-y-8">
       <img 
-        src="/bye_cat.svg" // Utilizzo del file SVG creato
+        src="/cat_bye.png" // Utilizzo dell'immagine PNG del gatto con gli occhiali
         alt="Gatto che saluta" 
-        className="w-72 h-72 object-contain" // Dimensioni maggiori per mostrare meglio l'immagine SVG
+        className="w-72 h-72 object-contain" // Mantengo le stesse dimensioni
       />
       <Link to="/"> {/* Link alla home page */} 
         <Button size="lg">Torna alla Home</Button>
