ce69bb8c7a7f051caa516e6667f2c58a08a0409f
Il Tuo Nome <la-tua-email@example.com>
2025-04-16 16:08:26 +0200

    feat: Aggiungi immagine personalizzata SVG del gatto con occhiali alla pagina Bye

--- src/pages/Bye.tsx
+++ src/pages/Bye.tsx
@@ -6,9 +6,9 @@ const Bye: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-10rem)] space-y-8">
       <img 
-        src="/loggedin_sentimentvers.png" // Stessa immagine di Welcome
-        alt="Bye Cat" 
-        className="w-48 h-48 object-contain" // Stesse dimensioni
+        src="/bye_cat.svg" // Utilizzo del file SVG creato
+        alt="Gatto che saluta" 
+        className="w-72 h-72 object-contain" // Dimensioni maggiori per mostrare meglio l'immagine SVG
       />
       <Link to="/"> {/* Link alla home page */} 
         <Button size="lg">Torna alla Home</Button>
